bf1aff3e8ad3dddee14a1ba1e03cd9a1df3f3de6
sudipkarki123 <sudipkarkicoding@gmail.com>
2024-12-16 18:45:08 +0545

    lesson 5

---
+++ main.py
@@ -0,0 +1,22 @@
+# a=int(input("Enter the number:") )
+# if a==0:
+#  print("the no is zero")
+# elif a>0:
+#   print("the no is positive")
+# else:
+#   print("the no is negative")
+
+# Buyingprice=int(input("Enter the Buyingprice="))
+# sellingprice=int(input("Enter the sellingprice="))
+# if Buyingprice==sellingprice:
+#   print("no profit and loss")
+# elif Buyingprice>sellingprice:
+#     print("Loss","Loss amount=",Buyingprice-sellingprice)
+# else:
+#      print("profit","profit amount=",sellingprice-Buyingprice)
+
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print(f"{num} is even")
+else:
+    print(f"{num} is odd")
